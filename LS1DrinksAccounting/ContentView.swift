b6a61ecbda21f778c742f68435a9b7bb93064ef2
Martin Fink <martin@finkmartin.com>
2022-12-10 12:11:23 +0100

    Initial Commit

---
+++ LS1DrinksAccounting/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LS1DrinksAccounting
+//
+//  Created by Martin Fink on 10.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LS1DrinksAccounting/LS1DrinksAccountingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LS1DrinksAccountingApp.swift
+//  LS1DrinksAccounting
+//
+//  Created by Martin Fink on 10.12.22.
+//
+
+import SwiftUI
+
+@main
+struct LS1DrinksAccountingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

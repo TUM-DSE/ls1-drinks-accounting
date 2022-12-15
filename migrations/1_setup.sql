ee37009437d3bcee50575d8a2edfcac05b967726
Martin Fink <martin@finkmartin.com>
2022-12-15 14:19:26 +0100

    Added setup sql migration

--- migrations/1_setup.sql
+++ migrations/1_setup.sql
@@ -1,8 +1,51 @@
+create
+extension if not exists "uuid-ossp";
+
 create table users
 (
-    id         UUID primary key not null,
-    first_name text             not null,
-    last_name  text             not null,
-    email      text             not null,
+    id         UUID primary key         not null default uuid_generate_v4(),
+    first_name text                     not null,
+    last_name  text                     not null,
+    email      text                     not null,
+    registered timestamp with time zone not null default now()
 );
 
+create table drinks
+(
+    id     UUID primary key not null default uuid_generate_v4(),
+    icon   text             not null default '',
+    name   text             not null,
+    price  int              not null,
+    hidden boolean          not null default false
+);
+
+create table deposits
+(
+    id     UUID primary key not null default uuid_generate_v4(),
+    "user" UUID             not null,
+    date   timestamp        not null default now(),
+    amount int              not null,
+
+    foreign key ("user") references users
+);
+
+create table transactions
+(
+    id     UUID primary key not null default uuid_generate_v4(),
+    "user" UUID             not null,
+    date   timestamp        not null default now(),
+    drink  UUID             not null,
+
+    foreign key (drink) references drinks
+);
+
+create view balances as
+select "user", sum(amount)
+from (select deposits.user as "user", deposits.amount as amount
+      from deposits
+
+      union all
+      select transactions.user as "user", 0 - drinks.price as amount
+      from transactions
+               inner join drinks on drinks.id = transactions.drink) as all_transactions
+group by "user";

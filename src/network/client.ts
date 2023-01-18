95ed4e1d06fa6fdd32da14dd96ae59e58981e4a4
Martin Fink <martin@finkmartin.com>
2023-01-18 21:34:06 +0100

    Get api endpoint url from .env

--- src/network/client.ts
+++ src/network/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_ENDPOINT,
   headers: {
     "Content-Type": "application/json",
   },

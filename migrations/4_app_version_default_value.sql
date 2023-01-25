8855ea5e38aa75b52f9c8fdbbd5326bd2c9a9d0c
Martin Fink <martin@finkmartin.com>
2023-01-25 21:24:40 +0100

    Add is_latest app version endpoint

---
+++ migrations/4_app_version_default_value.sql
@@ -0,0 +1,2 @@
+INSERT INTO app_configuration(id, value)
+VALUES ('app_version', NULL);
--- src/http/app_configuration.rs
+++ src/http/app_configuration.rs
@@ -2,29 +2,28 @@ use crate::db;
 use crate::http::errors::ApiError;
 use crate::http::ApiContext;
 use crate::types::auth::AuthUser;
-use axum::extract::State;
+use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::{Json, Router};
-use serde::Serialize;
-
-#[derive(Serialize)]
-pub struct AppVersionResponse {
-    version: Option<String>,
-}
+use semver::Version;
 
 pub async fn app_version(
     _user: AuthUser,
+    Path(requested_version): Path<Version>,
     State(state): State<ApiContext>,
 ) -> Result<impl IntoResponse, ApiError> {
-    let version = db::app_configuration::get_app_version(&state.db)
+    let is_latest = db::app_configuration::get_app_version(&state.db)
         .await?
-        .and_then(|config| config.value);
+        .and_then(|config| config.value)
+        .and_then(|version| Version::parse(&version).ok())
+        .map(|latest_version| requested_version >= latest_version)
+        .unwrap_or(true);
 
-    Ok((StatusCode::OK, Json(AppVersionResponse { version })))
+    Ok((StatusCode::OK, Json(is_latest)))
 }
 
 pub fn router() -> Router<ApiContext> {
-    Router::new().route("/api/config/app/version", get(app_version))
+    Router::new().route("/api/config/app/:version/is_latest", get(app_version))
 }

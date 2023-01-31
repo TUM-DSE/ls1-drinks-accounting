2da5f51618d8abe3360f207cc86ff67f30ae71c8
Martin Fink <martin@finkmartin.com>
2023-01-31 14:20:05 +0100

    Group transactions by date

--- LS1DrinksAccounting/Views/TransactionsView.swift
+++ LS1DrinksAccounting/Views/TransactionsView.swift
@@ -20,22 +20,24 @@ struct TransactionsView: View {
         return formatter
     }()
     
+    var transactionGroupedByDate: [Date: [Transaction]] {
+        Dictionary(grouping: viewModel.transactions, by: { Calendar.current.startOfDay(for: $0.timestamp) })
+    }
+    
+    var headers: [Date] {
+        transactionGroupedByDate.map { $0.key }.sorted().reversed()
+    }
+    
     var body: some View {
         ZStack {
             if viewModel.isLoading {
                 ProgressView()
             } else {
-                List(viewModel.transactions) { transaction in
-                    HStack {
-                        if case .moneyDeposit = transaction.transactionType {
-                            Label("Deposit", systemImage: "eurosign.circle")
+                List {
+                    ForEach(headers, id: \.self) { date in
+                        Section(header: sectionHeader(for: date)) {
+                            section(for: date)
                         }
-                        if case let .purchase(transactionData) = transaction.transactionType {
-                            Label(title: { Text(transactionData.purchase.name) }, icon: { Text(transactionData.purchase.icon) })
-                        }
-                        Text(formatter.string(from: NSNumber(value: transaction.amount)) ?? "")
-                        Spacer()
-                        Text(transaction.timestamp, style: .date)
                     }
                 }
             }
@@ -44,6 +46,36 @@ struct TransactionsView: View {
             Task {
                 await viewModel.loadTransactions()
             }
+        }
+    }
+    
+    func sectionHeader(for date: Date) -> some View {
+        Group {
+            if Calendar.current.startOfDay(for: date) == Calendar.current.startOfDay(for: Date()) {
+                Text("today")
+            } else {
+                Text(date, style: .date)
+            }
+        }
+    }
+    
+    func section(for date: Date) -> some View {
+        ForEach(transactionGroupedByDate[date]!) { transaction in
+            item(for: transaction)
+        }
+    }
+    
+    func item(for transaction: Transaction) -> some View {
+        HStack {
+            if case .moneyDeposit = transaction.transactionType {
+                Label("Deposit", systemImage: "eurosign.circle")
+            }
+            if case let .purchase(transactionData) = transaction.transactionType {
+                Label(title: { Text(transactionData.purchase.name) }, icon: { Text(transactionData.purchase.icon) })
+            }
+            Text(formatter.string(from: NSNumber(value: transaction.amount)) ?? "")
+            Spacer()
+            Text(transaction.timestamp, style: .time)
         }
     }
     
